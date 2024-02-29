c1dece3e0eda498d1c5fa10e398a5f979c54c6ba
Jmond544 <jmondalgotapia@gmail.com>
2024-02-29 16:41:49 -0500

    Remove unused import statement

--- src/components/sidebar/listItems.jsx
+++ src/components/sidebar/listItems.jsx
@@ -1,5 +1,4 @@
 import { FaRegCalendarCheck } from "react-icons/fa6";
-import { FaCashRegister } from "react-icons/fa6";
 import { FaBuildingCircleCheck } from "react-icons/fa6";
 import { FaOutdent } from "react-icons/fa6";
 
